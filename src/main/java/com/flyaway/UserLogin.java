3d6c7add5d7a93759eeabf03e2dcd4cc1fc62a9a
Mujahith <mujujaycees@gmail.com>
2022-08-22 14:01:30 +0530

    Final Commit

---
+++ src/main/java/com/flyaway/UserLogin.java
@@ -0,0 +1,49 @@
+package com.flyaway;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.URegistration.dao.Logindao;
+
+/**
+ * Servlet implementation class ULogin
+ */
+@WebServlet("/UserLogin")
+public class UserLogin extends HttpServlet {
+	private static final long serialVersionUID =1L;
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		
+		PrintWriter out = response.getWriter();
+		
+		//out.println(email+" "+pass);
+		Logindao logindao = new Logindao();
+	
+		if(logindao.check(name,email)){
+			
+			
+			response.sendRedirect("SearchFlight.jsp");
+			
+			
+		}
+		else{
+			
+			out.println("User Not Logged In");
+			
+		}
+		 
+		 
+		
+		
+	}
+
+}
